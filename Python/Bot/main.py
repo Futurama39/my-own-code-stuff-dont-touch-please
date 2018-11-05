731ed523242e05e6a33371eb256cd27ec81e1edb
Futurama39 <jchlubny@gmail.com>
2018-11-05 20:21:52 +0100

    simple help interaction and other cool stuff

--- Python/Bot/main.py
+++ Python/Bot/main.py
@@ -1,22 +1,23 @@
 import discord
-from discord.ext.commands import Bot
-from discord.ext import commands
-import asyncio
+import time
 
-Client = discord.Client()
-client = commands.bot
+client = discord.Client()
 
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
 
-
-
-
-
-
-
-
+    if message.content == "help":
+        await message.channel.send('No help is coming')
+        time.sleep(3)
+        await message.channel.send('Run.')
 
 with open('token.txt','r') as Token:
     token = Token.read().replace('\n','')
 
-Client.run(token)
+client.run(token)
