85153e0f096e458cc5034b11dbfec21e8b884329
Futurama39 <jchlubny@gmail.com>
2018-11-05 23:51:56 +0100

    Responding now working with the self assigned list

--- Python/Bot/main.py
+++ Python/Bot/main.py
@@ -1,7 +1,20 @@
 import discord
 import time
+import re
 
 client = discord.Client()
+
+lc = 0
+
+responses = [[]]
+
+def niceappend(uid,response):
+    global lc
+    row = [uid,response,0,0]
+    responses.append([])
+    responses[lc].extend(row)
+    lc = lc + 1
+    return True
 
 @client.event
 async def on_ready():
@@ -12,10 +25,24 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    if message.content == "whome":
+        await message.channel.send(message.author.id)
+
     if message.content == "help":
         await message.channel.send('No help is coming')
         time.sleep(3)
         await message.channel.send('Run.')
+        
+    if message.content.startswith("♥res "):
+        p_response = re.sub('♥res ','',message.content)
+        if niceappend(message.author.id,p_response):
+            await message.channel.send("Got it")
+        else:
+            await message.channel.send("Error lol")
+
+    if message.content == "showme":
+        await message.channel.send(responses)
+
 
 with open('token.txt','r') as Token:
     token = Token.read().replace('\n','')
