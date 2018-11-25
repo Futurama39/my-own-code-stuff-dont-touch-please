91fcd7cc591142b7a16b5ddc383de64ba5d868b7
Futurama39 <jchlubny@gmail.com>
2018-11-25 19:38:51 +0100

    exception handling for some stuff

--- Python/Bot/main.py
+++ Python/Bot/main.py
@@ -8,6 +8,13 @@ client = discord.Client()
 admins = ['195606469792497696']
 lc = 0
 
+class SavingError(Exception):
+    pass
+
+class DumpingError(Exception):
+    pass
+
+
 def is_admin(uid):
     if str(uid) in admins:
         return True
@@ -29,9 +36,9 @@ def dump():
         return False
 
 def save(i=0):
-    with open('responses.txt','w') as a:
-        a.write(json.dumps(responses))
-    return True
+        with open('responses.txt','w') as a:
+            a.write(json.dumps(responses))
+        return True
 
 def respond(uid,response):
     response_p = re.sub('♥res ','',response)
@@ -58,69 +65,101 @@ async def on_ready():
 @client.event
 async def on_message(message):
 
-    global responses
-    send = message.channel.send
-    ath = message.author
-    uid = message.author.id
-    start = message.content.startswith
-    msg = message.content
+    try:
+        global responses
+        send = message.channel.send
+        ath = message.author
+        uid = message.author.id
+        start = message.content.startswith
+        msg = message.content
 
-    if ath == client.user:
-        return
+        if ath == client.user:
+            return
     
-    if msg == "amadmin":
-        if is_admin(uid):
-            await send("Yes")
-        else:
-            await send("No")
-        return
+        if msg == "amadmin":
+            if is_admin(uid):
+                await send("Yes")
+            else:
+                await send("No")
+            return
 
-    if start("makeadmin") and is_admin(uid):
-        admin = re.sub("makeadmin ",'',msg)
-        admins.append(admin)
-        print(str(uid)+ " made " + str(admin) + " admin")
-        await send("Made <@" + str(uid)+"> Admin!")
+        if start("makeadmin"):
+            if is_admin(uid):
+                admin = re.sub("makeadmin ",'',msg)
+                admins.append(admin)
+                print(str(uid)+ " made " + str(admin) + " admin")
+                await send("Made <@" + str(uid)+"> Admin!")
+            else:
+                raise PermissionError()
 
 
-    if msg == "whome":
-        await send(uid)
-        return
+        if msg == "whome":
+            await send(uid)
+            return
 
-    if msg == "pingme":
-        await send("<@"+ str(uid)+">")
-        return
+        if msg == "pingme":
+            await send("<@"+ str(uid)+">")
+            return
         
-    if start("♥res "):
-        if not responded(uid) or is_admin(uid):
-            if respond(uid,msg):
-                await send("Got it")
-                save()
-                await send("Words: " + str(len(str.split(msg))-1))
-        else:
-            await send("You already responded once!")
-        return
+        if start("♥res "):
+            if not responded(uid) or is_admin(uid):
+                if respond(uid,msg):
+                    await send("Got it")
+                    try:
+                        save()
+                    except:
+                        raise SavingError
+                    await send("Words: " + str(len(str.split(msg))-1))
+            else:
+                await send("You already responded once!")
+            return
 
-    if msg == "showme" and is_admin(uid):
-        await send(responses)
-        return
+        if msg == "showme":
+            if is_admin(uid):
+                await send(responses)
+            else:
+                raise PermissionError()
+            return
     
-    if msg == "save" and is_admin(uid):
-        if save():
-            await send("Saved")
-        else:
-            await send("Failed to save")
-        return
+        if msg == "save":
+            if is_admin(uid):
+                save()
+            else:
+                raise PermissionError()
+            return
 
-    if msg == "dump" and is_admin:
-        if dump():
-            global lc
-            lc = 0
-            responses = [[]]
-            await send("Dumped!")
-            print("Responses dumped by: " + str(uid))
-        else:
-            await send("Something went wrong!")
-        return
+        if msg == "dump":
+            if is_admin(uid):
+                try:
+                    dump()
+                    global lc
+                    lc = 0
+                    responses = [[]]
+                    await send("Dumped!")
+                    print("Responses dumped by: " + str(uid))
+                except:
+                    raise DumpingError()
+            return
+
+    except PermissionError:
+        print(str(uid)+ "tried to acces an admin command")
+        await send("Not enough permissions!")
+
+    except SavingError:
+        print("Saving Error!")
+        print("Responses = "+ str(responses)+ "\n user = "+ str(uid) + "\n message = " + str(msg))
+        await send("Saving Error")
+
+    except DumpingError:
+        print("Dumping error!")
+        print(" Responses = "+str(responses)+ "\n lc = " + str(lc)+ "\n message = "+str(msg)+ "\n user = "+str(uid))
+        await send("Dumping Error!")
+
+    except:
+        print(" Responses = "+str(responses)+ "\n lc = " + str(lc)+ "\n message = "+str(msg)+ "\n user = "+str(uid))
+        print("Unknown error!")
+        await send("Unknown error!")
+
 
 
 
