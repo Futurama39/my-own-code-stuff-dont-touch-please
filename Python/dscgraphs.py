e638aecd933f66355ae47b85672b882b45ec36b0
Futurama39 <jchlubny@gmail.com>
2020-11-08 13:12:11 +0100

    improvement on #11, still bugs present

--- Python/dscgraphs.py
+++ Python/dscgraphs.py
@@ -4,14 +4,11 @@ import numpy as np
 import re
 
 import dscmsg
-
-table = dscmsg.chart_get('C:\\Users\\Uzivatel\\Documents\\di_exports\\new4\\',True)
-
-vani = [r'starshooter',r'a7598201',r'b547650a']
-cassady = [r'aRADia',r'aradia temporary account']
-littlepage = [r'The Neolithian',r'6a739393']
+dscmsg.choosefile()
+table = dscmsg.chart_get()
 
 def combine(lst):
+    #take two rows and combine them
     global table
     temptable = []
     global table
@@ -37,17 +34,11 @@ def comparison(lst): #takes in lists of row names to be displayed
     return outtable
 
 
-combine(vani)
-combine(cassady)
-combine(littlepage)
-table.sort()
-table = comparison([16,9,12,13,14,18])
-
-
 if __name__ == "__main__":
-    x_axis = range(len(table[0])-1)
+    del table[0][0]
+    x_axis = range(len(table[0]))
     ax = plt.gca()
-    for line in table:
-        plt.plot(x_axis,line[1:],label=str(line[0]))
+    for line in table[1:]:
+        plt.plot(x_axis,line,label=str(line[0]))
     plt.legend()
     plt.show()
