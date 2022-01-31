4f3e876c2d6a7bf0c5700e248b19928c441a2391
Futurama39 <jchlubny@gmail.com>
2022-01-31 22:58:44 +0100

    this outs html files for further usage

--- graph.py
+++ graph.py
@@ -5,7 +5,14 @@ pd.options.plotting.backend = "plotly"
 
 def main(df: pd.DataFrame) -> None:
     fig = df.plot.line()
-    fig.show()
+    while True:
+        i = 0
+        try:
+            file_cand = f'out{i}.html'
+            fig.write_html(file_cand)
+            exit()
+        except FileExistsError:
+            i += 1
 
 
 if __name__ == '__main__':
