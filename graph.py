4c01d337701c92450ca56ad2e8d62171fb82098a
Futurama39 <jchlubny@gmail.com>
2022-02-01 00:06:11 +0100

    get os to check for created files so we don't overwrite

--- graph.py
+++ graph.py
@@ -1,17 +1,18 @@
 import pandas as pd
 import table
+import os
 pd.options.plotting.backend = "plotly"
 
 
 def main(df: pd.DataFrame) -> None:
     fig = df.plot.line()
+    i = 0
     while True:
-        i = 0
-        try:
-            file_cand = f'out{i}.html'
+        file_cand = f'out{i}.html'
+        if not os.path.isfile(file_cand):
             fig.write_html(file_cand)
             exit()
-        except FileExistsError:
+        else:
             i += 1
 
 
